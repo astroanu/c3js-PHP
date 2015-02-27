f995c022bcec5356ee87587597aa578a154da042
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-02-27 11:40:52 +0530

    Line chart option methods

--- src/Charts/Line.php
+++ src/Charts/Line.php
@@ -2,7 +2,25 @@
 
 class Line implements \JsonSerializable {
 
+	const STEP_TYPE_STEP = 'step';
+	const STEP_TYPE_STEP_BEFORE = 'step-before';
+	const STEP_TYPE_STEP_AFTER = 'step-after';
+
 	private $data = [];
+
+	public function setStepType($type)
+	{
+		if (!isset($this->data['step'])) {
+			$this->data['step'] = [];
+		}
+
+		$this->data['step']['type'] = $type;
+	}
+
+	public function setConnectNull($connect)
+	{
+		$this->data['connectNull'] = $connect;
+	}
 
 	public function JsonSerialize()
     {
