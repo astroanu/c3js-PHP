d434b6de667d47e0a8c4d127ded84ece56561fe2
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-02-27 11:41:44 +0530

    Pie chart option methods

--- src/Charts/Pie.php
+++ src/Charts/Pie.php
@@ -4,6 +4,29 @@ class Pie implements \JsonSerializable {
 
 	private $data = [];
 
+	public function setExpand($expand)
+	{
+		$this->data['expand'] = $expand;
+	}
+
+	public function setLabeltThreshold($threshold)
+	{
+		if (!isset($this->data['label'])) {
+			$this->data['label'] = [];
+		}
+		
+		$this->data['label']['threshold'] = $threshold;
+	}	
+
+	public function setLabelVisibility($visibility)
+	{
+		if (!isset($this->data['label'])) {
+			$this->data['label'] = [];
+		}
+
+		$this->data['label']['show'] = $visibility;
+	}
+
 	public function JsonSerialize()
     {
         return $this->data;
