b2baf1d9eb475c16737991859ead965a712e5e24
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-02-27 10:05:24 +0530

    chart class

--- src/Data.php
+++ src/Data.php
@@ -2,7 +2,51 @@
 
 class Data implements \JsonSerializable {
 
+	const TYPE_LINE = 'line';
+	const TYPE_SPLINE = 'spline';
+	const TYPE_STEP = 'step';
+	const TYPE_AREA = 'area';
+	const TYPE_AREA_SPLINE = 'area-spline';
+	const TYPE_AREA_STEP = 'area-step';
+	const TYPE_BAR = 'bar';
+	const TYPE_SCATTER = 'scatter';
+	const TYPE_PIE = 'pie';
+	const TYPE_DONUT = 'donut';
+	const TYPE_GAUGE = 'gauge';
+
 	private $data = [];
+
+	public function setKeysValue($fields)
+	{
+		if(!isset($this->data['keys'])){
+			$this->data['keys'] = [];
+		}
+		$this->data['keys']['value'] = $fields;
+	}
+
+	public function setType($type)
+	{
+		$this->data['type'] = $type;
+	}
+
+	public function setNames($names)
+	{
+		$this->data['names'] = $names;
+	}
+
+	public function setXFormat($format)
+	{
+		$this->data['xFormat'] = $format;
+	}
+
+	public function setX($x)
+	{
+		if (is_array($x)) {
+			$this->data['x'] = $x;
+		} else {
+			$this->data['xs'] = $x;
+		}
+	}
 
 	public function setColumns($data)
 	{
