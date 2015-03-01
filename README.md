c93b7ceed72f1ceb9507d372bc03859bcbea6260
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-03-01 19:24:57 +0530

    data class docs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # c3js-PHP
+
+See demos directory for sample code
+
+[http://astroanu.github.io/c3js-PHP] for API reference (not upto date)
--- src/Data.php
+++ src/Data.php
@@ -16,6 +16,14 @@ class Data implements \JsonSerializable {
 
 	private $data = [];
 
+    /**
+    * Set keys for x axis when axis x is on category type
+    *
+    * @param string $field
+    *
+    * @return Data
+    *
+    */
 	public function setKeysX($field)
 	{
 		if (!isset($this->data['keys'])) {
@@ -23,55 +31,137 @@ class Data implements \JsonSerializable {
 		}
 
 		$this->data['keys']['x'] = $field;
+		return $this;
 	}
 
+    /**
+    * Set which json object keys correspond to which data
+    *
+    * @param array $fields
+    *
+    * @return Data
+    *
+    */
 	public function setKeysValue($fields)
 	{
 		if (!isset($this->data['keys'])) {
 			$this->data['keys'] = [];
 		}
 
-		$this->data['keys']['value'] = $fields;
+		$this->data['keys']['value'] = $field;
+		return $this;
 	}
 
-	public function setType($type, $options = null)
+    /**
+    * Set chart type
+    *
+    * @param TYPE_LINE|TYPE_SPLINE|TYPE_STEP|TYPE_AREA|TYPE_AREA_SPLINE|TYPE_AREA_STEP|TYPE_BAR|TYPE_SCATTER|TYPE_PIE|TYPE_DONUT|TYPE_GAUGE $type
+    *
+    * @return Data
+    *
+    */
+	public function setType($type)
 	{
 		$this->data['type'] = $type;
+		return $this;
 	}
 
+    /**
+    * Set custom data name
+    *
+    * @param array $names
+    *
+    * @return Data
+    *
+    */
 	public function setNames($names)
 	{
 		$this->data['names'] = $names;
+		return $this;
 	}
 
+    /**
+    * Set format to parse x
+    *
+    * @param string $format
+    *
+    * @return Data
+    *
+    */
 	public function setXFormat($format)
 	{
 		$this->data['xFormat'] = $format;
+		return $this;
 	}
 
+    /**
+    * Set key of x values in data
+    *
+    * @param string $x
+    *
+    * @return Data
+    *
+    */
 	public function setX($x)
 	{
 		$this->data['x'] = $x;
+		return $this;	
 	}
 
+    /**
+    * Set chart data as columns
+    *
+    * @param array $data
+    *
+    * @return Data
+    *
+    */
 	public function setColumns($data)
 	{
 		$this->data['columns'] = $data;
+		return $this;
 	}
 
+    /**
+    * Set chart data as rows
+    *
+    * @param array $data
+    *
+    * @return Data
+    *
+    */
 	public function setRows($data)
 	{
 		$this->data['rows'] = $data;
+		return $this;
 	}
 
-	public function setJson($data)
+    /**
+    * Set chart data from json
+    *
+    * @param array $data
+    *
+    * @return Data
+    *
+    */
+    public function setJson($data)
 	{
 		$this->data['json'] = $data;
+		return $this;
 	}
 
+    /**
+    * Set chart data from a json or csv file
+    *
+    * @param string $url
+    *
+    * @return Data
+    *
+    */
 	public function setUrl($url)
 	{
 		$this->data['url'] = $url;
+		return $this;
 	}
 
 	public function JsonSerialize()
