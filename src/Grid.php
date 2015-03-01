13e8bbe50a6299667b892edb829509cbc0b346d9
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-03-01 19:30:27 +0530

    grid class docs

--- src/Grid.php
+++ src/Grid.php
@@ -4,6 +4,48 @@ class Grid implements \JsonSerializable {
 
 	private $data = [];
 
+    /**
+    * Set additional grid lines along x
+    *
+    * @param array $lines
+    *
+    * @return Grid
+    *
+    */
+	public function setXLines($lines)
+	{
+		if (!isset($this->data['x'])) {
+			$this->data['x'] = [];
+		}
+
+		$this->data['x']['lines'] = $lines;
+	}
+
+    /**
+    * Set additional grid lines along y 
+    *
+    * @param array $lines
+    *
+    * @return Grid
+    *
+    */
+	public function setYLines($lines)
+	{
+		if (!isset($this->data['y'])) {
+			$this->data['y'] = [];
+		}
+
+		$this->data['y']['lines'] = $lines;
+	}
+
+    /**
+    * Set visibility of grid liness along y axis
+    *
+    * @param boolean $visibility
+    *
+    * @return Grid
+    *
+    */
 	public function setYVisibility($visibility)
 	{
 		if (!isset($this->data['y'])) {
@@ -13,7 +55,16 @@ class Grid implements \JsonSerializable {
 		$this->data['y']['show'] = $visibility;
 	}
 
-	public function setXVisibility($visibility)
+
+    /**
+    * Set visibility of grid liness along x axis
+    *
+    * @param boolean $visibility
+    *
+    * @return Grid
+    *
+    */
+    public function setXVisibility($visibility)
 	{
 		if (!isset($this->data['x'])) {
 			$this->data['x'] = [];
