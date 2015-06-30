8a6eacc770b57d1ec0293ca9e4c459fbef7de290
Anuradha Jayathilaka <astroanu2004@gmail.com>
2015-06-30 09:27:55 +0530

    legend params

--- src/Legend.php
+++ src/Legend.php
@@ -4,6 +4,16 @@ class Legend implements \JsonSerializable {
 
 	private $data = [];
 
+	public function setPosition($position)
+	{
+		$this->data['position'] = $position;		
+	}
+
+	public function setVisibility($visibility)
+	{
+		$this->data['show'] = $visibility;
+	}
+
 	public function JsonSerialize()
     {
         return $this->data;
