6678cc1db4d505a4f5261a31fd8d915f78db9a1e
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-08-04 06:24:14 +0200

    Completed Gauge object

--- src/Charts/Gauge.php
+++ src/Charts/Gauge.php
@@ -1,44 +1,123 @@
-<?php namespace Astroanu\C3jsPHP\Charts;
+<?php
+/*
+ * Copyright (C) 2015 Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
+ */
 
-class Gauge implements \JsonSerializable {
+namespace Astroanu\C3jsPHP\Charts;
 
-	const TYPE_TIMESERIES = 'timeseries';
-	const TYPE_CATEGORY = 'category';
-	const TYPE_INDEXED = 'indexed';
+/**
+ * Class Gauge
+ * @package Astroanu\C3jsPHP\Charts
+ */
+class Gauge implements \JsonSerializable
+{
+    const TYPE_TIMESERIES = 'timeseries';
+    const TYPE_CATEGORY = 'category';
+    const TYPE_INDEXED = 'indexed';
 
-	private $data = [];
+    /**
+     * @var array
+     */
+    private $data = [];
 
-	public function setWidth($width)
-	{
-		$this->data['width'] = $width;
-	}
+    /**
+     * Show or hide label on gauge
+     *
+     * @param bool $visibility
+     *
+     * @link http://c3js.org/reference.html#gauge-label-show
+     */
+    public function setLabelVisibility($visibility = true)
+    {
+       $this->ensureLabel();
+        $this->data['label']['show'] = $visibility;
+    }
 
-	public function setUnits($units)
-	{
-		$this->data['units'] = $units;
-	}
+    /**
+     * Set formatter for the label on gauge
+     *
+     * @param string $format
+     *
+     * @link http://c3js.org/reference.html#gauge-label-format
+     */
+    public function setLabelFormat($format)
+    {
+        $this->ensureLabel();
+        $this->data['label']['format'] = $format;
+    }
 
-	public function setMin($min)
-	{
-		$this->data['min'] = $min;
-	}
 
-	public function setExpand($expand)
-	{
-		$this->data['expand'] = $expand;
-	}
+    /**
+     * Enable or disable expanding gauge
+     *
+     * @param bool $expand
+     *
+     * @link http://c3js.org/reference.html#gauge-expand
+     */
+    public function setExpand($expand = true)
+    {
+        $this->data['expand'] = $expand;
+    }
 
-	public function setLabelVisibility($visibility)
-	{
-		if (!isset($this->data['label'])) {
-			$this->data['label'] = [];
-		}
+    /**
+     * Set min value of the gauge
+     *
+     * @param int $min
+     *
+     * @link http://c3js.org/reference.html#gauge-min
+     */
+    public function setMin($min = 0)
+    {
+        $this->data['min'] = $min;
+    }
 
-		$this->data['label']['show'] = $visibility;
-	}
+    /**
+     * Set max value of the gauge
+     *
+     * @param int $max
+     *
+     * @link http://c3js.org/reference.html#gauge-max
+     */
+    public function setMax($max = 100)
+    {
+        $this->data['max'] = $max;
+    }
 
-	public function JsonSerialize()
+    /**
+     * Set units of the gauge
+     *
+     * @param string $units
+     *
+     * @link http://c3js.org/reference.html#gauge-units
+     */
+    public function setUnits($units)
+    {
+        $this->data['units'] = $units;
+    }
+
+    /**
+     * Set width of gauge chart
+     * @param int $width
+     *
+     * @link http://c3js.org/reference.html#gauge-width
+     */
+    public function setWidth($width)
+    {
+        $this->data['width'] = $width;
+    }
+
+    /**
+     * @return array
+     */
+    public function JsonSerialize()
     {
         return $this->data;
+    }
+
+    private function ensureLabel()
+    {
+        if (!isset($this->data['label'])) {
+            $this->data['label'] = [];
+        }
     }
 }
