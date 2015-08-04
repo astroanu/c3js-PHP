0c5b65559f60f26e1759c1bb1287ab7dfb6feaa7
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-08-04 06:16:15 +0200

    Completed Pie object

--- src/Charts/Pie.php
+++ src/Charts/Pie.php
@@ -1,34 +1,85 @@
-<?php namespace Astroanu\C3jsPHP\Charts;
+<?php
+/*
+ * Copyright (C) 2015 Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
+ */
 
-class Pie implements \JsonSerializable {
+namespace Astroanu\C3jsPHP\Charts;
 
-	private $data = [];
+/**
+ * Class Pie
+ * @package Astroanu\C3jsPHP\Charts
+ */
+class Pie implements \JsonSerializable
+{
+    /**
+     * @var array
+     */
+    private $data = [];
 
-	public function setExpand($expand)
-	{
-		$this->data['expand'] = $expand;
-	}
+    /**
+     * Show or hide label on each pie piece
+     *
+     * @param $visibility
+     *
+     * @link http://c3js.org/reference.html#pie-label-show
+     */
+    public function setLabelVisibility($visibility = true)
+    {
+        $this->ensureLabel();
+        $this->data['label']['show'] = $visibility;
+    }
 
-	public function setLabeltThreshold($threshold)
-	{
-		if (!isset($this->data['label'])) {
-			$this->data['label'] = [];
-		}
-		
-		$this->data['label']['threshold'] = $threshold;
-	}	
+    /**
+     * Set formatter for the label on each pie piece
+     *
+     * @param string $format
+     *
+     * @link http://c3js.org/reference.html#pie-label-format
+     */
+    public function setLabelFormat($format)
+    {
+        $this->ensureLabel();
+        $this->data['label']['format'] = $format;
+    }
 
-	public function setLabelVisibility($visibility)
-	{
-		if (!isset($this->data['label'])) {
-			$this->data['label'] = [];
-		}
 
-		$this->data['label']['show'] = $visibility;
-	}
+    /**
+     * Set threshold to show/hide labels
+     *
+     * @param float $threshold
+     *
+     * @link http://c3js.org/reference.html#pie-label-threshold
+     */
+    public function setLabeltThreshold($threshold = 0.05)
+    {
+        $this->ensureLabel();
+        $this->data['label']['threshold'] = $threshold;
+    }
 
-	public function JsonSerialize()
+    /**
+     * Enable or disable expanding pie pieces
+     *
+     * @param bool $expand
+     *
+     * @link http://c3js.org/reference.html#pie-expand
+     */
+    public function setExpand($expand = true)
+    {
+        $this->data['expand'] = $expand;
+    }
+
+    /**
+     * @return array
+     */
+    public function JsonSerialize()
     {
         return $this->data;
+    }
+
+    private function ensureLabel()
+    {
+        if (!isset($this->data['label'])) {
+            $this->data['label'] = [];
+        }
     }
 }
