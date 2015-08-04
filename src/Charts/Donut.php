9a9ce88adca91f9e367fe16f0c9da3e0a76c6757
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2015-08-04 06:19:49 +0200

    Completed Donut object

--- src/Charts/Donut.php
+++ src/Charts/Donut.php
@@ -1,44 +1,108 @@
-<?php namespace Astroanu\C3jsPHP\Charts;
+<?php
+/*
+ * Copyright (C) 2015 Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
+ */
 
-class Donut implements \JsonSerializable {
+namespace Astroanu\C3jsPHP\Charts;
 
-	private $data = [];
+/**
+ * Class Donut
+ * @package Astroanu\C3jsPHP\Charts
+ */
+class Donut implements \JsonSerializable
+{
+    /**
+     * @var array
+     */
+    private $data = [];
 
-	public function setTitle($title)
-	{
-		$this->data['title'] = $title;
-	}
+    /**
+     * Show or hide label on each donut piece
+     *
+     * @param $visibility
+     *
+     * @link http://c3js.org/reference.html#donut-label-show
+     */
+    public function setLabelVisibility($visibility = true)
+    {
+        $this->ensureLabel();
+        $this->data['label']['show'] = $visibility;
+    }
 
-	public function setWidth($width)
-	{
-		$this->data['width'] = $width;
-	}
+    /**
+     * Set formatter for the label on each donut piece
+     *
+     * @param string $format
+     *
+     * @link http://c3js.org/reference.html#donut-label-format
+     */
+    public function setLabelFormat($format)
+    {
+        $this->ensureLabel();
+        $this->data['label']['format'] = $format;
+    }
 
-	public function setExpand($expand)
-	{
-		$this->data['expand'] = $expand;
-	}
+    /**
+     * Set threshold to show/hide labels
+     *
+     * @param float $threshold
+     *
+     * @link http://c3js.org/reference.html#donut-label-threshold
+     */
+    public function setLabelThreshold($threshold = 0.05)
+    {
+        $this->ensureLabel();
+        $this->data['label']['threshold'] = $threshold;
+    }
 
-	public function setLabeltThreshold($threshold)
-	{
-		if (!isset($this->data['label'])) {
-			$this->data['label'] = [];
-		}
-		
-		$this->data['label']['threshold'] = $threshold;
-	}	
+    /**
+     * Enable or disable expanding donut pieces
+     *
+     * @param bool $expand
+     *
+     * @link http://c3js.org/reference.html#donut-expand
+     */
+    public function setExpand($expand = true)
+    {
+        $this->data['expand'] = $expand;
+    }
 
-	public function setLabelVisibility($visibility)
-	{
-		if (!isset($this->data['label'])) {
-			$this->data['label'] = [];
-		}
+    /**
+     * Set width of donut chart
+     *
+     * @param int $width
+     *
+     * @link http://c3js.org/reference.html#donut-width
+     */
+    public function setWidth($width)
+    {
+        $this->data['width'] = $width;
+    }
 
-		$this->data['label']['show'] = $visibility;
-	}
+    /**
+     * Set title of donut chart
+     *
+     * @param string $title
+     *
+     * @link http://c3js.org/reference.html#donut-title
+     */
+    public function setTitle($title = '')
+    {
+        $this->data['title'] = $title;
+    }
 
-	public function JsonSerialize()
+    /**
+     * @return array
+     */
+    public function JsonSerialize()
     {
         return $this->data;
+    }
+
+    private function ensureLabel()
+    {
+        if (!isset($this->data['label'])) {
+            $this->data['label'] = [];
+        }
     }
 }
